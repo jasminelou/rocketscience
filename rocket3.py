f7a85b1b6f6817273bdbc7e0e76a4ce7cef5766a
jasminelou <yajielou@hanovernorwichschools.org>
2018-03-15 08:27:55 -0400

    Updated from Brython Server: 3/15/2018 8:27:54 AM

--- rocket3.py
+++ rocket3.py
@@ -1,1 +1,32 @@
+from ggrocket import *
+from math import *
+from ggmath import *
 
+earth = Planet(planetmass=0)
+
+RocketStarted = False
+StartTime = None
+BurnTime = 0
+
+me = 25600
+mp = 395700
+F1D = 716000
+NID = 9
+Ftotal = F1D * N1D
+tburn = 180
+
+vmax = Ftotal*tburn/(me+mp)
+print("Predicted final velocity (Newton's 2nd Law), vmax: ", vmax, " m/s")
+
+def GetThrust():
+    global BurnTurn, RocketStarted
+    if RocketStarted:
+        BurnTime = rocket.shiptime - StartTime
+        if BurnTime >= tburn:
+            RocketStarted = False
+            return 0
+        return Ftotal
+    return 0
+    
+def StartRocket():
+    global RocketStarted
